04aa99606b74dacfc2a10af0c2e5db7b5cf63901
KJB <kjb_jh@naver.com>
2024-06-04 12:20:25 +0900

    2D camera, spring arm C++ added.
    # 2D 카메라, 스프링 암 C++추가.

---
+++ CreateGameProject/Source/CreateGameProject/SideScroll/SideScrollCharacter.cpp
@@ -0,0 +1,53 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SideScroll/SideScrollCharacter.h"
+#include "GameFramework/SpringArmComponent.h" // 스프링 암.
+#include "Camera/CameraComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "Components/CapsuleComponent.h"
+
+// Sets default values
+ASideScrollCharacter::ASideScrollCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// 스프링 암
+	SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraCommponent"));
+	
+	CameraComponent->SetProjectionMode(ECameraProjectionMode::Orthographic);
+	// 2D
+	CameraComponent->OrthoWidth = 1280.0f * 2;
+
+	// SpringArmComponent->SetupAttachment(RootComponent);
+	// RootComponent == GetRootComponent()
+	SpringArmComponent->SetupAttachment(GetCapsuleComponent());
+	CameraComponent->SetupAttachment(SpringArmComponent);
+
+	FQuat Rot = UKismetMathLibrary::Quat_MakeFromEuler(FVector(0.0f, 0.0f, -90.0f));
+	SpringArmComponent->SetRelativeRotation(Rot);
+}
+
+// Called when the game starts or when spawned
+void ASideScrollCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASideScrollCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ASideScrollCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
